705fd0535ece0edf365b21f77576bc754c00b9d8
devxb <devxb@naver.com>
2022-04-24 07:40:16 +0000

    add : KeywordParser

--- fingerslave/src/main/java/finger/slave/service/parser/KeywordParser.java
+++ fingerslave/src/main/java/finger/slave/service/parser/KeywordParser.java
@@ -2,13 +2,21 @@ package finger.slave.service.parser;
 
 import java.util.*;
 
+import org.springframework.stereotype.Service;
+
 import finger.slave.service.util.*;
 
+@Service
 public class KeywordParser implements FingerParser{
 	
 	@Override
 	public Map<String, ParameterWrapper> parse(String content, String... keywords){
-		return new HashMap();
+		Map<String, ParameterWrapper> keywordMap = new HashMap<String, ParameterWrapper>();
+		for(String keyword : keywords){
+			Integer patternCount = PatternCounter.count(content, keyword);
+			keywordMap.put(keyword, ParameterWrapper.wrap(patternCount));
+		}
+		return keywordMap;
 	}
 	
 }
--- fingerslave/src/test/java/finger/slave/service/parser/FingerParserTest.java
+++ fingerslave/src/test/java/finger/slave/service/parser/FingerParserTest.java
@@ -10,10 +10,11 @@ import finger.slave.service.util.*;
 
 public class FingerParserTest{
 	
-	private HyperLinkParser hyperLinkParser = new HyperLinkParser();
+	private FingerParser hyperLinkParser = new HyperLinkParser();
+	private FingerParser keywordParser = new KeywordParser();
 	
 	@Test
-	public void Parse_HyperLinks_Google_Test(){
+	public void Parse_HyperLinks_Test(){
 		// given
 		String uri = "https://test.domain";
 		String content = this.getContent();
@@ -25,8 +26,28 @@ public class FingerParserTest{
 		assertTrue(!hyperLinkMap.isEmpty());
 	}
 	
+	@Test
+	public void Parse_Keyword_Test(){
+		// given
+		String content = this.getContent();
+		
+		// when
+		Map<String, ParameterWrapper> keywordMap = this.keywordParser.parse(content, "채용", "가입", "recruit", "Join", "join", "인턴", "모집", "신청");
+		
+		// then
+		assertTrue(!keywordMap.isEmpty());
+		assertEquals(keywordMap.get("채용").unWrap(), 4);
+		assertEquals(keywordMap.get("가입").unWrap(), 3);
+		assertEquals(keywordMap.get("신청").unWrap(), 0);
+		assertEquals(keywordMap.get("인턴").unWrap(), 2);
+		assertEquals(keywordMap.get("recruit").unWrap(), 1);
+		assertEquals(keywordMap.get("Join").unWrap(), 2);
+		assertEquals(keywordMap.get("join").unWrap(), 1);
+		assertEquals(keywordMap.get("모집").unWrap(), 3);
+	}
+	
 	private String getContent(){
-		return "<!DOCTYPE.HTML><HTML><head><title>test-title</title></head><body><a href='https://test.domain/test-url' /></body></HTML>";
+		return "<!DOCTYPE.HTML><HTML><head><title>test-title</title></head><body><a href='https://test.domain/test-url'/> <pre>채용 채용 채용 recruit 채용 가입 가입 Join 가입 Join join 인턴 모집 모집 인턴 모집 </pre></body></HTML>";
 	}
 	
 }
